828401f691cef75da63b6dfb11de29891d3a1134
phongpnt <50826077+phongpnt@users.noreply.github.com>
2026-08-03 10:32:46 +0700

    Add files via upload

---
+++ source/mobile/smart_home_app/README.md
@@ -0,0 +1,17 @@
+# Smart Home App
+
+## Create by Zillur Rahman
+
+## Description
+This is Smart Home App UI Design.
+
+## Flutter 3.22.2
+
+Load thư viện về chạy lệnh
+flutter pub get
+
+khi run trên android thì chạy lệnh
+flutter run 
+
+khi build app ra apk
+flutter build apk --release
---
+++ source/mobile/smart_home_app/ios/Flutter/ephemeral/flutter_lldb_helper.py
@@ -0,0 +1,21 @@
+import lldb
+
+def handle_new_rx_page(frame: lldb.SBFrame, bp_loc, extra_args, intern_dict):
+    base = frame.register["x0"].GetValueAsAddress()
+    page_len = frame.register["x1"].GetValueAsUnsigned()
+
+    data = bytearray(page_len)
+    data[0:8] = b'IHELPED!'
+
+    error = lldb.SBError()
+    frame.GetThread().GetProcess().WriteMemory(base, data, error)
+    if not error.Success():
+        print(f'Failed to write into {base}[+{page_len}]', error)
+        return
+
+def __lldb_init_module(debugger: lldb.SBDebugger, _):
+    target = debugger.GetDummyTarget()
+    bp = target.BreakpointCreateByRegex("^NOTIFY_DEBUGGER_ABOUT_RX_PAGES$")
+    bp.SetScriptCallbackFunction('{}.handle_new_rx_page'.format(__name__))
+    bp.SetAutoContinue(True)
+    print("-- LLDB integration loaded --")
